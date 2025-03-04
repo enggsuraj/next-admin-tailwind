063dad71b18b2d24ce7b07469f82868b986ffe91
suraj <suraj@Zibrans-MacBook-Pro.local>
2025-03-04 23:42:31 +0530

    fix/toggle-theme

--- src/app/(admin)/page.tsx
+++ src/app/(admin)/page.tsx
@@ -26,9 +26,6 @@ export default function Ecommerce() {
         <MonthlyTarget />
       </div>
 
-      <div className="col-span-12">
-        <StatisticsChart />
-      </div>
 
       <div className="col-span-12 xl:col-span-5">
         <DemographicCard />
@@ -37,6 +34,11 @@ export default function Ecommerce() {
       <div className="col-span-12 xl:col-span-7">
         <RecentOrders />
       </div>
+
+      <div className="col-span-12">
+        <StatisticsChart />
+      </div>
+      
     </div>
   );
 }
